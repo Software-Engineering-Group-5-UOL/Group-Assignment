ae4a458e81200e4faf70c5189f2414f140528b02
Ryan Grange <rgrange@protonmail.com>
2019-10-31 14:24:53 +0000

    Added index

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<head>
+    <title>Headline Music</title>
+    <meta property="og:title" content="Headline Music" />
+    <meta property="og:url" content="https://software.ryangrange.co.uk/" />
+    <meta property="og:locale" content="en_GB" />
+    <meta property="og:description" content="Our artifact from the software engeneering module." />
+</head>
+<body>
+    <section class="content">
+        <h1>Headline Music</h1>
+        <h1>this is some new content</h1>
+        <h1>this is some additional new content</h1>
+        <?php
+
+
+
+
+
+
+
+
+
+        ?>
+    </section>
+</body>

720e54725e6b298eead4bd58cc9e8b4fa54177ae
AND17644224 <56123551+AND17644224@users.noreply.github.com>
2019-11-07 16:34:40 +0000

    Update forgotten.php

--- forgotten.php
+++ forgotten.php
@@ -81,16 +81,18 @@ if( $_GET["key"]) {
                         
                         if($stmt = mysqli_prepare($link, $sql)){
                             // Bind variables to the prepared statement as parameters
-                            mysqli_stmt_bind_param($stmt, "iisi", NULL, NULL, $param_usr_password, $param_id);
+                            mysqli_stmt_bind_param($stmt, "iisi", $param_tempuse, $param_temptype, $param_usr_password, $param_id);
                             
                             // Set parameters
                             $param_usr_password = password_hash($new_password, PASSWORD_DEFAULT);
                             $param_id = $_SESSION["id"];
+                            $param_tempuse = NULL;
+                            $param_temptype = NULL;
                 
                             //execute statement
                             if(mysqli_stmt_execute($stmt)){
                                 // Clear the database fields that are for tempoary use
-                                $sql1 = "UPDATE users SET tempuse = NULL, temptype = NULL, usr_password = $usr_password WHERE id = $id";
+                                $sql1 = "UPDATE users SET tempuse = $tempuse, temptype = $temptype, usr_password = $usr_password WHERE id = $id";
                                 
                                 if (mysqli_query($link, $sql1)) {
                                     header("location: index.php");

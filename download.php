6a703274000418fb7fb9675388e28f8c4ff767b0
Ryan <rgrange@pm.me>
2019-11-21 15:03:56 +0000

    started to add the download page and apps

---
+++ download.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'pageelements/header.php';
+?>
+
+<a href="./desktopapps/Headline Music for Windows.exe" download>Windows Version</a>
+
+
+<?php
+include_once 'pageelements/footer.php';
+?>

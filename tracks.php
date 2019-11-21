2553e50c86c797148f452ed237206c6fefb85fe4
AND17644224 <56123551+AND17644224@users.noreply.github.com>
2019-11-21 14:30:15 +0000

    Update tracks.php

--- tracks.php
+++ tracks.php
@@ -18,22 +18,22 @@ if((!isset($_GET["token"]) || empty($_GET["token"])) && (!isset($_GET["code"]) |
 elseif(isset($_GET["code"]) && !empty($_GET["code"])) {
 //    $url = 'https://accounts.spotify.com/api/token';
     $code = $_GET["code"];
-    echo $code; 
-}
-//        $data = array('grant_type' => 'authorization_code', 'code' => $code, 'redirect_uri' => 'https://headlinemusicapp.co.uk/tracks.php');
 
-//        // use key 'http' even if you send the request to https://...
-//        $options = array(
-//            'http' => array(
-//                'header'  => "Content-type: application/x-www-form-urlencoded\r\n"
-//                 ."Authorization: Basic ".$clientid.":".$clientsecret."\r\n",
-//                'method'  => 'POST',
-//                'content' => http_build_query($data)
-//            )
-//        );
-//        $context  = stream_context_create($options);
-//        $result = file_get_contents($url, false, $context);
-//        if ($result === FALSE) { /* Handle error */ }
+       $data = array('grant_type' => 'authorization_code', 'code' => $code, 'redirect_uri' => 'https://headlinemusicapp.co.uk/tracks.php');
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n"
+                 ."Authorization: Basic ".$clientid.":".$clientsecret."\r\n",
+                'method'  => 'POST',
+              'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) { /* Handle error */ }
+}
  //} elseif(isset($_GET["token"] && !empty($_GET["token"])) {
  //  echo "I'm here last else!!";
  //} else {

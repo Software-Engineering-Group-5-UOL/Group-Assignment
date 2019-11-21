abf1d741a142c0a1dcd9f70508648fb705b461c9
AND17644224 <56123551+AND17644224@users.noreply.github.com>
2019-11-21 13:47:16 +0000

    Update tracks.php

--- tracks.php
+++ tracks.php
@@ -12,8 +12,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: index.php");
     exit;
 }
-if((!isset($_GET["token"]) || empty($_GET["token"])) || (!isset($_GET["code"]) || empty($_GET["code"]))){
-    header("location: https://accounts.spotify.com/authorize?client_id=".$clientid."&response_type=code&redirect_uri=https%3A%2F%2Fheadlinemusicapp.co.uk%2Ftracks.php&scope=user-read-private%20playlist-modify-private&state=".$session_id);
+// if((!isset($_GET["token"]) || empty($_GET["token"])) || (!isset($_GET["code"]) || empty($_GET["code"]))){
+//     header("location: https://accounts.spotify.com/authorize?client_id=".$clientid."&response_type=code&redirect_uri=https%3A%2F%2Fheadlinemusicapp.co.uk%2Ftracks.php&scope=user-read-private%20playlist-modify-private&state=".$session_id);
 // } else {
 //     if(isset($_GET["code"] && !empty($_GET["code"])) {
 //        $url = 'https://accounts.spotify.com/api/token';
